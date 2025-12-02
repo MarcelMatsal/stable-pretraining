5c0871ff351d12d44c0630019b421e3eacfd349c
marcel_mateos_salles@brown.edu <mmateoss@gpu2001.oscar.ccv.brown.edu>
2025-12-01 22:51:35 -0500

    updating training script

--- examples/clip_runs.sh
+++ examples/clip_runs.sh
@@ -20,11 +20,11 @@ fi
 for seed in 40; do
     for lora_rank in 0; do
         for proportion in 1; do
-            for patch_size in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1; do
+            for spur_alpha in 0.01 0.02 0.05 0.1 0.25 0.5 0.75 1; do
 
                 echo "Running with alpha=$alpha, lora_rank=$lora_rank, proportion=$proportion, seed=$seed"
 
-                python clip_finetuning.py ++params.dataset=uoft-cs/cifar10 ++params.label_key=label ++params.zeroshot_dataset=uoft-cs/cifar10 ++params.zero_label=label ++params.use_lora=False ++params.lora_rank=$lora_rank ++params.spur_proportion=$proportion ++params.patch_size=$patch_size ++params.use_spurious=True "$@"
+                python clip_finetuning.py ++params.dataset=uoft-cs/cifar10 ++params.label_key=label ++params.zeroshot_dataset=uoft-cs/cifar10 ++params.zero_label=label ++params.spur_type=checkerboard ++params.use_lora=False ++params.lora_rank=$lora_rank ++params.spur_proportion=$proportion ++params.spur_alpha=$spur_alpha ++params.use_spurious=True "$@"
 
             done
         done
